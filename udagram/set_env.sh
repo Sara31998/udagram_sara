4151df0c2d994775c3016bb36b1e0e7bc4c31b8d
Sara Gamal <sg837121@gmail.com>
2023-03-04 13:13:19 +0200

    udagram_Udacity_project

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udag_sara
+export POSTGRES_PASSWORD=saraud123456
+export POSTGRES_HOST=udagramsara.cqpqjwxdbyue.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramsara
+export AWS_BUCKET=arn:aws:s3:::sara-udagram123456
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=token-youer-secret
+export URL=http://localhost:3000
---
+++ udagram/udagram-api/src/sequelize.ts
@@ -0,0 +1,14 @@
+import { Sequelize } from "sequelize-typescript";
+import { config } from "./config/config";
+
+export const sequelize = new Sequelize({
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+  port: config.port_dp,
+  dialect: "postgres",
+  storage: "postgres:memory:",
+  logging: false
+});
+
